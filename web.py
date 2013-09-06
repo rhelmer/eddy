c212aaacc0007ce4da25de6727b962f7ea07e16b
Robert Helmer <rhelmer@rhelmer.org>
2013-09-06 03:35:06 -0700

    provide way to get status of job

--- web.py
+++ web.py
@@ -9,13 +9,13 @@ app.debug = True
 @app.route('/perf/startup')
 def startup():
     appname = flask.request.args.get('appname', '')
-    uuid = tasks.perftest.delay(appname)
-    return 'queued %s' % uuid
+    task_id = tasks.perftest.delay(appname)
+    return 'queued as task ID: %s' % task_id
 
-@app.route('/perf/work')
-def work():
-    worker_name = flask.request.args.get('name', '')
-    return worker_name
+@app.route('/perf/status')
+def status():
+    task_id = flask.request.args.get('task_id', '')
+    return tasks.celery.AsyncResult(task_id).state
 
 if __name__ == '__main__':
     app.run()
