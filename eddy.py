aae933c904298e333cc4c7395e352a5b057b7160
Robert Helmer <rhelmer@rhelmer.org>
2013-09-05 23:53:02 -0700

    get things basically working

--- eddy.py
+++ eddy.py
@@ -7,12 +7,13 @@ import subprocess
 import tempfile
 
 BASEURL='https://marketplace.firefox.com/api/v1/apps/app'
-APPNAME='stopwatch-1'
+ADB_PATH='/Users/rhelmer/src/android-platform-tools/'
 
 def loadApp(appname):
     market_request = requests.get('%s/%s' % (BASEURL, appname))
     logging.info('requesting app manifest from marketplace')
     manifest_url = market_request.json()['manifest_url']
+    name = market_request.json()['name']
     market_request.close()
 
     manifest_request = requests.get(manifest_url)
@@ -27,13 +28,34 @@ def loadApp(appname):
             if chunk:
                 temp.write(chunk)
                 temp.flush()
-        logging.info('FIXME load %s onto phone' % temp.name)
+        # TODO actually load onto phone...
+        logging.info('TODO actually load %s onto phone...' % temp.name)
+
+    return name
 
 def testApp(appname):
     logging.info('forward port for marionette')
-    subprocess.check_call(['adb', 'shell', 'forward', 'tcp:2828', 'tcp:2828'])
-    logging.info('run b2gperf')
-    subprocess.check_call(['env/bin/python', 'env/bin/b2gperf', appname])
+    try:
+        subprocess.check_call(['%s/adb' % ADB_PATH, 'shell',
+                               'forward','tcp:2828', 'tcp:2828'])
+    except subprocess.CalledProcessError, e:
+        logging.error('adb failed with error code: %s' % e.returncode)
+        logging.error('check that the phone is connected?')
+        raise
+
+    logging.info('run b2gperf with app: %s' % appname)
+    # FIXME should this be importing b2gperf directly instead?
+    # TODO switch to subprocess.check_output on python2.7+
+    p = subprocess.Popen(['env/bin/python', 'env/bin/b2gperf', appname],
+                         stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    out, err = p.communicate()
+    returncode = p.returncode
+
+    if returncode == 0:
+        return out
+    else:
+        raise Exception('b2gperf failed, return code: %s stdout/err: %s / %s'
+                        % (returncode, out, err))
 
 def main():
     logging.basicConfig(level=logging.INFO)
@@ -49,12 +71,12 @@ def main():
     args = parser.parse_args()
 
     if args.download_only:
-        loadApp(APPNAME)
+        loadApp(args.appname)
     elif args.test_only:
-        testApp(APPNAME)
+        testApp(args.appname)
     else:
-        loadApp(APPNAME)
-        testApp(APPNAME)
+        name = loadApp(args.appname)
+        testApp(name)
 
 if __name__ == '__main__':
     main()
