1efcb64df677c077bd156e43063f0d950b2bb8ca
Robert Helmer <rhelmer@rhelmer.org>
2013-09-19 10:11:09 -0700

    clearer distinction between app slug and app name

--- eddy.py
+++ eddy.py
@@ -12,11 +12,11 @@ BASEURL='https://marketplace.firefox.com/api/v1/apps/app'
 #BASEURL='http://localhost:8000/api/v1/apps/app'
 ADB_PATH='/Users/rhelmer/src/android-platform-tools/'
 
-def loadApp(appname):
-    market_request = requests.get('%s/%s' % (BASEURL, appname))
+def loadApp(app_slug):
+    market_request = requests.get('%s/%s' % (BASEURL, app_slug))
     logging.info('requesting app manifest from marketplace')
     manifest_url = market_request.json()['manifest_url']
-    name = market_request.json()['name']
+    app_name = market_request.json()['name']
     market_request.close()
 
     manifest_request = requests.get(manifest_url)
@@ -26,7 +26,7 @@ def loadApp(appname):
 
     logging.info('create appdir on device')
     subprocess.check_call(['adb', 'shell', 'mkdir',
-        '/data/local/webapps/%s' % appname])
+        '/data/local/webapps/%s' % app_slug])
 
     with tempfile.NamedTemporaryFile() as temp:
         for chunk in manifest_request.iter_content(chunk_size=1024):
@@ -42,7 +42,7 @@ def loadApp(appname):
                 f.write(json.dumps(manifest_json))
         logging.info('load manifest onto device')
         subprocess.check_call(['adb', 'push', temp.name,
-            '/data/local/webapps/%s/manifest.webapp' % appname])
+            '/data/local/webapps/%s/manifest.webapp' % app_slug])
 
     app_request = requests.get(app_url, stream=True)
     logging.info('downloading app')
@@ -53,30 +53,30 @@ def loadApp(appname):
                 temp.flush()
         logging.info('load app onto device')
         subprocess.check_call(['adb', 'push', temp.name,
-            '/data/local/webapps/%s/application.zip' % appname])
+            '/data/local/webapps/%s/application.zip' % app_slug])
 
     subprocess.check_call(['adb', 'pull',
         '/data/local/webapps/webapps.json', '.'])
     with open('webapps.json', 'r') as f:
         webapps = json.loads(f.read())
 
-    if appname in webapps:
-        del webapps[appname]
+    if app_slug in webapps:
+        del webapps[app_slug]
 
     localId = max([v['localId'] for k,v in webapps.iteritems()]) + 1
 
-    webapps[appname] = {
-        'origin': 'app://%s' % appname,
-        'installOrigin': 'app://%s' % appname,
-        'manifestURL': 'app://%s/manifest.webapp' % appname,
+    webapps[app_slug] = {
+        'origin': 'app://%s' % app_slug,
+        'installOrigin': 'app://%s' % app_slug,
+        'manifestURL': 'app://%s/manifest.webapp' % app_slug,
         'appStatus': 1,
         'installTime': 1379012730464,
         'installState': 'installed',
         'removable': True,
-        'id': appname,
+        'id': app_slug,
         'basePath': '/data/local/webapps',
         'localId': localId,
-        'name': 'Stopwatch'
+        'name': app_name,
     }
 
     with open('webapps-new.json', 'w') as f:
@@ -85,15 +85,15 @@ def loadApp(appname):
     subprocess.check_call(['adb', 'push', 'webapps-new.json',
         '/data/local/webapps/webapps.json'])
 
-    return name
+    return app_name
 
-def testApp(appname):
+def testApp(app_name, app_slug):
     logging.info('forward port for marionette')
     subprocess.check_call(['adb', 'forward','tcp:2828', 'tcp:2828'])
-    logging.info('run b2gperf with app: %s' % appname)
+    logging.info('run b2gperf with app: %s' % app_name)
     # FIXME should this be importing b2gperf directly instead?
     # TODO switch to subprocess.check_output on python2.7+
-    p = subprocess.Popen(['env/bin/python', 'env/bin/b2gperf', appname,
+    p = subprocess.Popen(['env/bin/python', 'env/bin/b2gperf', app_name,
                           '--settle-time=%s' % settings.SETTLE_TIME,
                           '--iterations=%s' % settings.ITERATIONS,
                           '--dz-project=%s' % settings.DZ_PROJECT,
@@ -128,8 +128,8 @@ def main():
     elif args.test_only:
         testApp(args.appname)
     else:
-        name = loadApp(args.appname)
-        testApp(name)
+        app_name, app_slug = loadApp(args.appname)
+        testApp(app_name, app_slug)
 
 if __name__ == '__main__':
     main()
